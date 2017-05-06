c85d58edbb6d20b91ffc85df1f132f78a3ddf062
Robert Homan <Bobhoman25@gmail.com>
2017-05-06 14:01:04 -0400

    Project start
    Project creation - first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const video = document.getElementById('myVideo');
+const capContainer = document.querySelector('.captions-div');
+const captions = document.querySelectorAll('p span');
+
+video.addEventListener('timeupdate', () => {
+  for (let i = 0; i < captions.length; i += 1) {
+    let time = video.currentTime;
+    let start = captions[i].getAttribute('data-start');
+    let end = captions[i].getAttribute('data-end');
+
+    if (time >= start && time <= end) {
+        captions[i].className = "highlighted";
+    } else {
+    	captions[i].className = "";
+    }
+  }
+});
+
+capContainer.addEventListener('click', (event) => {
+  if (event.target.tagName == 'SPAN') {
+  	let curTime = event.target.getAttribute('data-start');
+  	video.currentTime = curTime;
+  }
+});
+
